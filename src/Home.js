2f5045bebcd062a601cb1c2f6e8fd23d293947e1
Kaitlin <kaitlinjwalsh@gmail.com>
2019-12-19 22:16:21 +1100

    added snow

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Home.css';
 import { Link } from 'react-router-dom';
+import Snowfall from 'react-snowfall';
 export default function Home({ removeFromLocalStorage }) {
+
     return (
         <div className="home-page">
-            <canvas className="canvas"></canvas>
+            <div className="snow">
+                <Snowfall />
+            </div>
             <h1>Build An Elf!</h1>
             <Link onClick={() => removeFromLocalStorage()} className="btn btn-2 btn-2g" to="/build">Let's start!</Link>
         </div>
